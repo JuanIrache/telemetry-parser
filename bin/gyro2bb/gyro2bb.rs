20557af0312440f278c488bda57090234ae2110a
Juan Irache <yrache@gmail.com>
2025-09-05 03:06:36 +0200

    no CSV if dump

--- bin/gyro2bb/gyro2bb.rs
+++ bin/gyro2bb/gyro2bb.rs
@@ -53,39 +53,39 @@ fn main() {
                 }
             }
         }
-    }
-
-    let imu_data = util::normalized_imu(&input, opts.imuo).unwrap();
-
-    let mut csv = String::with_capacity(2*1024*1024);
-    csv.push_str(r#""Product","Blackbox flight data recorder by Nicholas Sherlock""#);
-    csv.push('\n');
-    crate::try_block!({
-        let map = samples.get(0)?.tag_map.as_ref()?;
-        let json = (map.get(&GroupId::Default)?.get_t(TagId::Metadata) as Option<&serde_json::Value>)?;
-        for (k, v) in json.as_object()? {
-            csv.push('"');
-            csv.push_str(&k.to_string());
-            csv.push_str("\",");
-            csv.push_str(&v.to_string());
+    } else {
+            let imu_data = util::normalized_imu(&input, opts.imuo).unwrap();
+            
+            let mut csv = String::with_capacity(2*1024*1024);
+            csv.push_str(r#""Product","Blackbox flight data recorder by Nicholas Sherlock""#);
             csv.push('\n');
+            crate::try_block!({
+                let map = samples.get(0)?.tag_map.as_ref()?;
+                let json = (map.get(&GroupId::Default)?.get_t(TagId::Metadata) as Option<&serde_json::Value>)?;
+                for (k, v) in json.as_object()? {
+                    csv.push('"');
+                    csv.push_str(&k.to_string());
+                    csv.push_str("\",");
+                    csv.push_str(&v.to_string());
+                    csv.push('\n');
+                }
+            });
+            
+            csv.push_str(r#""loopIteration","time","gyroADC[0]","gyroADC[1]","gyroADC[2]","accSmooth[0]","accSmooth[1]","accSmooth[2]""#);
+            csv.push('\n');
+            for v in imu_data {
+                if v.gyro.is_some() || v.accl.is_some() {
+                    let gyro = v.gyro.unwrap_or_default();
+                    let accl = v.accl.unwrap_or_default();
+                    csv.push_str(&format!("{},{:.0},{},{},{},{},{},{}\n", i, (v.timestamp_ms * 1000.0).round(),
+                    -gyro[2], gyro[1], gyro[0],
+                    -accl[2] * 2048.0, accl[1] * 2048.0, accl[0] * 2048.0
+                ));
+                i += 1;
+            }
         }
-    });
-
-    csv.push_str(r#""loopIteration","time","gyroADC[0]","gyroADC[1]","gyroADC[2]","accSmooth[0]","accSmooth[1]","accSmooth[2]""#);
-    csv.push('\n');
-    for v in imu_data {
-        if v.gyro.is_some() || v.accl.is_some() {
-            let gyro = v.gyro.unwrap_or_default();
-            let accl = v.accl.unwrap_or_default();
-            csv.push_str(&format!("{},{:.0},{},{},{},{},{},{}\n", i, (v.timestamp_ms * 1000.0).round(),
-                -gyro[2], gyro[1], gyro[0],
-                -accl[2] * 2048.0, accl[1] * 2048.0, accl[0] * 2048.0
-            ));
-            i += 1;
-        }
+        std::fs::write(&format!("{}.csv", std::path::Path::new(&opts.input).to_path_buf().to_string_lossy()), csv).unwrap();
     }
-    std::fs::write(&format!("{}.csv", std::path::Path::new(&opts.input).to_path_buf().to_string_lossy()), csv).unwrap();
 
     println!("Done in {:.3} ms", _time.elapsed().as_micros() as f64 / 1000.0);
 }
