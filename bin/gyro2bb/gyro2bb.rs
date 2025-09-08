ea1787f965271a4b9fa063f7ef2e7863459f346f
Juan Irache <yrache@gmail.com>
2025-09-08 19:41:02 +0200

    print normalised imu to json lines

--- bin/gyro2bb/gyro2bb.rs
+++ bin/gyro2bb/gyro2bb.rs
@@ -7,26 +7,23 @@ use std::sync::{ Arc, atomic::AtomicBool };
 
 use telemetry_parser::*;
 use telemetry_parser::tags_impl::*;
+use telemetry_parser::util; 
 
-/** gyro2bb v0.2.8
-Author: Adrian <adrian.eddy@gmail.com>
+use serde_json::json;
 
-Extract gyro data from Sony, GoPro and Insta360 cameras to betaflight blackbox csv log
-*/
+/** gyro2bb v0.2.8-Author: Adrian <adrian.eddy@gmail.com> Extract gyro and metadata from camera files **/
+
 #[derive(FromArgs)]
 struct Opts {
     /// input file
     #[argh(positional)]
     input: String,
 
-    /// dump all metadata
-    #[argh(switch, short = 'd')]
-    dump: bool,
-
     /// IMU orientation (XYZ, ZXY etc, lowercase is negative, eg. xZy)
     #[argh(option)]
     imuo: Option<String>,
 }
+
 
 fn main() {
     let opts: Opts = argh::from_env();
@@ -35,57 +32,64 @@ fn main() {
     let mut stream = std::fs::File::open(&opts.input).unwrap();
     let filesize = stream.metadata().unwrap().len() as usize;
 
-    let input = Input::from_stream(&mut stream, filesize, &opts.input, |_|(), Arc::new(AtomicBool::new(false))).unwrap();
+    let input = Input::from_stream(
+        &mut stream, 
+        filesize, 
+        &opts.input, 
+        |_|(), 
+        Arc::new(AtomicBool::new(false))
+    ).unwrap();
 
-    let mut i = 0;
-    println!("Detected camera: {} {}", input.camera_type(), input.camera_model().unwrap_or(&"".into()));
+    let camera_type = input.camera_type().to_string();
+    let camera_model = input.camera_model().unwrap_or(&"".into()).to_string();
 
     let samples = input.samples.as_ref().unwrap();
 
-    if opts.dump {
-        for info in samples {
-            if info.tag_map.is_none() { continue; }
-            let grouped_tag_map = info.tag_map.as_ref().unwrap();
+    // new "json-like" mode
+    let imu_data = util::normalized_imu(&input, opts.imuo).unwrap();
 
-            for (group, map) in grouped_tag_map {
-                for (tagid, taginfo) in map {
-                    println!("{: <25} {: <25} {: <50}: {}", format!("{}", group), format!("{}", tagid), taginfo.description, &taginfo.value.to_string());
-                }
+    // collect Extra metadata
+    let mut extra_metadata = None;
+    if let Some(first) = samples.get(0) {
+        if let Some(map) = &first.tag_map {
+            if let Some(extra) = map.get(&GroupId::Default)
+                                    .and_then(|g| g.get(&TagId::Metadata)) {
+                extra_metadata = Some(extra.value.to_string());
             }
         }
-    } else {
-            let imu_data = util::normalized_imu(&input, opts.imuo).unwrap();
-            
-            let mut csv = String::with_capacity(2*1024*1024);
-            csv.push_str(r#""Product","Blackbox flight data recorder by Nicholas Sherlock""#);
-            csv.push('\n');
-            crate::try_block!({
-                let map = samples.get(0)?.tag_map.as_ref()?;
-                let json = (map.get(&GroupId::Default)?.get_t(TagId::Metadata) as Option<&serde_json::Value>)?;
-                for (k, v) in json.as_object()? {
-                    csv.push('"');
-                    csv.push_str(&k.to_string());
-                    csv.push_str("\",");
-                    csv.push_str(&v.to_string());
-                    csv.push('\n');
-                }
-            });
-            
-            csv.push_str(r#""loopIteration","time","gyroADC[0]","gyroADC[1]","gyroADC[2]","accSmooth[0]","accSmooth[1]","accSmooth[2]""#);
-            csv.push('\n');
-            for v in imu_data {
-                if v.gyro.is_some() || v.accl.is_some() {
-                    let gyro = v.gyro.unwrap_or_default();
-                    let accl = v.accl.unwrap_or_default();
-                    csv.push_str(&format!("{},{:.0},{},{},{},{},{},{}\n", i, (v.timestamp_ms * 1000.0).round(),
-                    -gyro[2], gyro[1], gyro[0],
-                    -accl[2] * 2048.0, accl[1] * 2048.0, accl[0] * 2048.0
-                ));
-                i += 1;
-            }
-        }
-        std::fs::write(&format!("{}.csv", std::path::Path::new(&opts.input).to_path_buf().to_string_lossy()), csv).unwrap();
     }
 
-    println!("Done in {:.3} ms", _time.elapsed().as_micros() as f64 / 1000.0);
+    let camera = json!({
+        "camera": {
+            "type": camera_type,
+            "model": camera_model
+        }   ,
+        "extra_metadata": extra_metadata
+    });
+
+    println!("CAMERASTART");
+    println!("{}", serde_json::to_string_pretty(&camera).unwrap());
+    println!("CAMERAEND");
+    println!("IMUSTART");
+
+    // convert IMU into JSON array
+    let mut i = 0;
+    for v in imu_data {
+        if v.gyro.is_some() || v.accl.is_some() {
+            let gyro = v.gyro.unwrap_or_default();
+            let accl = v.accl.unwrap_or_default();
+            let imu = json!({
+                "t": v.timestamp_ms,
+                "gyro": { "x": -gyro[2], "y": gyro[1], "z": gyro[0] },
+                "accel": { "x": -accl[2] * 2048.0, "y": accl[1] * 2048.0, "z": accl[0] * 2048.0 }
+            });
+            println!("{}", serde_json::to_string(&imu).unwrap());
+            i += 1;
+        }
+    }
+
+    
+
+    println!("IMUEND");
+    
 }
